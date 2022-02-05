e785f1279b45beb985f31a8c65320b77aaf9e8a7
Makito Mori <mkt.it.engr@gmail.com>
2022-02-05 23:35:11 +0900

    finish

--- src/AlwaysSuspend.tsx
+++ src/AlwaysSuspend.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { Loadable } from "./Loadable";
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -26,4 +27,54 @@ export const RenderingNotifier: React.VFC<Props> = ({ name }) => {
   console.log(`${name} is rendered`);
 
   return null;
+};
+
+export async function fetchData1(): Promise<string> {
+  await sleep(Math.floor(Math.random() * 1000));
+  return `Hello, ${(Math.random() * 1000).toFixed(0)}`;
+}
+
+//toFixedは引数の桁分残す
+//let numObj = 12345.6789
+// numObj.toFixed(1)      // '12345.7' を返す : 四捨五入
+
+let data: string | undefined;
+
+const dataMap: Map<string, unknown> = new Map();
+
+export function useData<T>(cacheKey: string, fetch: () => Promise<T>): T {
+  const cachedData = dataMap.get(cacheKey) as T | undefined;
+  if (cachedData === undefined) {
+    throw fetch().then((d) => dataMap.set(cacheKey, d));
+  }
+  return cachedData;
+}
+
+export const DataLoader1: React.VFC = () => {
+  const data = useData("DataLoader1", fetchData1);
+  return (
+    <div>
+      <div>Data is {data}</div>
+    </div>
+  );
+};
+
+export const DataLoader2: React.VFC = () => {
+  const data = useData("DataLoader2", fetchData1);
+  return (
+    <div>
+      <div>Data is {data}</div>
+    </div>
+  );
+};
+
+export const DataLoader: React.VFC<{
+  data: Loadable<string>;
+}> = ({ data }) => {
+  const value = data.getOrThrow();
+  return (
+    <div>
+      <div>Data is {value}</div>
+    </div>
+  );
 };
